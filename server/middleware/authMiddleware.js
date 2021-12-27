c62048a4118394101ef5bbaff353f602974b397f
MikhailMamonov <mamon201071@gmail.com>
2021-12-27 12:11:42 +0300

    remove options request condition

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  if (req.method == "OPTIONS") {
-    next();
-  }
+  // if (req.method == "OPTIONS") {
+  //   next();
+  // }
 
   try {
     const token = req.headers.authorization.split(" ")[1];
